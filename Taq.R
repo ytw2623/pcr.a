9663c0dd3e4385aa0cd8f0f7e3944474e8b60741
ytw2623 <68214881+ytw2623@users.noreply.github.com>
2020-07-27 22:25:01 +0900

    Add files via upload

---
+++ Taq.R
@@ -0,0 +1,51 @@
+##### Taq
+error.t <- c(.040, .145, .730, .085)
+error.a <- c(.045, .170, .710, .075)
+x.t <- c(-2, -1, 0, 1)
+x.a <- c(-2, -1, 0, 1)
+n <- 100
+set.val <- 26
+
+
+
+### cycle 3
+res <- pcr.a.first(x.t, x.a, error.t, error.a, n, set.val) * 3
+t(res/sum(res))
+#par(cex.axis=1.8, mar=c(5,5,3,2), cex.lab=1.8, cex.main=1.8,
+# xaxs="i", yaxs="i")
+#barplot(res/sum(res), ylim=c(0,1), space=.1, las=2)
+#write.csv(t(res/sum(res)), "cycle-03.csv")
+
+
+
+### cycle 5
+cycles <- 1 # choose 1 for 5 cycles; 4 for 10; 6 for 15; 9 for 20; 11 for 25; 14 for 30; 16 for 35
+res.a <- pcr.a(x.t, x.a, error.t, error.a, cycles, n, set.val) * 3
+res.b <- pcr.a.first(x.t, x.a, error.t, error.a, n, set.val) * 7
+res <- res.a + res.b
+t(res/sum(res))
+#par(cex.axis=1.8, mar=c(5,5,3,2), cex.lab=1.8, cex.main=1.8,
+# xaxs="i", yaxs="i")
+#barplot(res/sum(res), ylim=c(0,1), space=.1, las=2)
+#write.csv(t(res/sum(res)), "cycle-05.csv")
+
+
+
+### cycle 10
+cycles <- 4 # choose 1 for 5 cycles; 4 for 10; 6 for 15; 9 for 20; 11 for 25; 14 for 30; 16 for 35
+res.a <- pcr.a(x.t, x.a, error.t, error.a, cycles, n, set.val)
+cycles <- 3 # choose 1 for 5 cycles; 4 for 10; 6 for 15; 9 for 20; 11 for 25; 14 for 30; 16 for 35
+res.b <- pcr.a(x.t, x.a, error.t, error.a, cycles, n, set.val) * 5
+cycles <- 2 # choose 1 for 5 cycles; 4 for 10; 6 for 15; 9 for 20; 11 for 25; 14 for 30; 16 for 35
+res.c <- pcr.a(x.t, x.a, error.t, error.a, cycles, n, set.val) * 9
+cycles <- 1 # choose 1 for 5 cycles; 4 for 10; 6 for 15; 9 for 20; 11 for 25; 14 for 30; 16 for 35
+res.d <- pcr.a(x.t, x.a, error.t, error.a, cycles, n, set.val) * 13
+res.e <- pcr.a.first(x.t, x.a, error.t, error.a, n, set.val) * 17
+res <- res.a + res.b + res.c + res.d + res.e
+t(res/sum(res))
+#par(cex.axis=1.8, mar=c(5,5,3,2), cex.lab=1.8, cex.main=1.8,
+# xaxs="i", yaxs="i")
+#barplot(res/sum(res), ylim=c(0,1), space=.1, las=2)
+#write.csv(t(res/sum(res)), "cycle-10.csv")
+
+
---
+++ pcr.a.first.R
@@ -0,0 +1,51 @@
+##### This pcr.a.first() is for calculating the cycles 2 & 3.
+pcr.a.first <- function(x.t = x.t, x.a = x.a, errot.t = error.t, error.a = error.a,
+ n = n, set.val=set.val)
+{
+     cycle.0 <- rep(set.val, n)
+     
+     # for cycle 1
+     res <- NA
+     for(j in 1:length(x.t)){
+          for(k in 1:length(table(cycle.0))){
+               res1 <- cbind(as.numeric(rownames(table(cycle.0)))[[k]] + x.t[j],
+                table(cycle.0)[[1]] * error.t[j])
+               res <- rbind(res, res1)
+          }
+     }
+     result <- res[-1,]
+     result <- as.data.frame(result)
+     result[rownames(subset(result, V1==set.val)),2] <- subset(result, V1==set.val)[2] + n
+
+     # for cycle 3 (identical to cycle 2 on another chain)
+     res <- NA
+     for(j in 1:dim(result)[1]){
+          for(k in 1:length(x.a)){
+               res1 <- cbind(result[j,1] + x.a[k], result[j,2] * error.a[k])
+               res <- rbind(res, res1)
+          }
+     }
+     result <- res[-1,]
+     result <- as.data.frame(result)
+
+     res1 <- 0
+     res <- 0
+     for(l in 1:length(table(result[,1]))){
+          res1 <- cbind(as.numeric(rownames(table(result[,1])))[l],
+           apply(subset(result, result[,1]==as.numeric(rownames(table(result[,1])))[l]), 2, sum)[2])
+          res <- rbind(res1, res)
+     }
+     rownames(res) <- res[,1]
+     res <- res[order(res[,1]),]
+     res <- res[-1,-1]
+
+     res2 <- matrix(rep(0,41), 1,41)
+     colnames(res2) <- 0:40
+     for(i in 1:length(res)){
+          for(j in 1:length(res2)){
+               if(colnames(res2)[j]==names(res)[i]) res2[j] <- res[[i]]
+          }
+     }
+
+     return(res2)
+}
